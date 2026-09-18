eb47cf0d12ff4ba7345f7187dc4618c50bcf49be
akshatcoderpro-arch <akshatcoderpro@gmail.com>
2026-09-18 19:12:00 +0530

    Delete Day8calculator.py

--- Day8calculator.py
+++
@@ -1,9 +0,0 @@
-#dAY 8 CALCULATOR SOLUTION
-
-a = int(input("enter your first number"))
-b = int(input("enter your second number"))
-
-print(f"Adition of {a} and {b} is {a+b}")
-print(f"Subtraction of {a} and {b} is {a-b}")
-print(f"Multiplication of {a} and {b} is {a*b}")
-print(f"Division of {a} and {b} is {a/b}")
